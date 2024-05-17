56a67c483329eaf480e164d039c2427b7741cd3c
Dorine Morin <112956859+Dorine-Morin@users.noreply.github.com>
2024-05-17 16:54:36 -0400

    save

--- Project/Assets/SceneJeu/Scripts/AgentController.cs
+++ Project/Assets/SceneJeu/Scripts/AgentController.cs
@@ -1,46 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Xsl;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class AgentController : Agent
 {
-    [SerializeField] private GameObject bouton;
-    [SerializeField] private List<GameObject> portes;
+    
     [SerializeField] private GameObject sol;
+    [SerializeField] private Material solMaterial;
 
-    private Transform boutonTransform;
-    private Transform porte1Transform;
-    private Transform porte2Transform;
+    [SerializeField] private GameObject interrupteur;
+    [SerializeField] private Transform interrupteurTransform;
+    [SerializeField] private Material interrupteurMaterial;
+    [SerializeField] private bool interrupteurAtteint;
 
-    private Material boutonMaterial;
-    private Material porte1Material;
-    private Material porte2Material;
-    private Material solMaterial;
+    [SerializeField] private List<GameObject> portes;
 
-    private bool porte1Ouverte = false;
-    private bool porte2Ouverte = false;
-    private bool boutonActif = true;
+    [SerializeField] private Transform porte1Transform;
+    [SerializeField] private Material porte1Material;
+    [SerializeField] private bool porte1Ouverte = false;
 
-    private GameObject porteToReach;
-    private Material porteToReachMat;
-    private int indexPorteToReach;
+    [SerializeField] private Transform porte2Transform;
+    [SerializeField] private Material porte2Material;
+    [SerializeField] private bool porte2Ouverte = false;
 
+    [SerializeField] private GameObject porteToReach;
+    [SerializeField] private Transform porteToReachTransform;
+    [SerializeField] private Material porteToReachMat;
+    [SerializeField] private int indexPorteToReach;
 
-    private void Start()
-    {
-        boutonMaterial = bouton.GetComponent<MeshRenderer>().material;
-        porte1Material = portes[0].GetComponent<MeshRenderer>().material;
-        porte2Material = portes[1].GetComponent<MeshRenderer>().material;
-        solMaterial = sol.GetComponent<MeshRenderer>().material;        
+    [SerializeField] private Color materialOn = Color.yellow;
+    [SerializeField] private Color materialOff = Color.black;
+    [SerializeField] private Color materialGameOver = Color.red;
+    [SerializeField] private Color materialGameSuccess = Color.green;
 
-        boutonTransform = bouton.GetComponent<Transform>();
-        porte1Transform = portes[0].GetComponent<Transform>();
-        porte2Transform = portes[1].GetComponent<Transform>();
-    }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
@@ -48,44 +46,33 @@ public class AgentController : Agent
         float moveY = actions.ContinuousActions[1];
         float speed = 5;
         transform.Translate(new Vector3(moveX, 0, moveY) * Time.deltaTime * speed);
-        
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(boutonTransform.localPosition);
-        sensor.AddObservation(porte1Transform.localPosition);
-        sensor.AddObservation(porte2Transform.localPosition);
-        sensor.AddObservation(boutonActif ? 1 : 0);
-        sensor.AddObservation(porte1Ouverte ? 1 : 0);
+
+        sensor.AddObservation(porte1Ouverte ? 1: 0);
         sensor.AddObservation(porte2Ouverte ? 1 : 0);
+
+        if (!interrupteurAtteint)
+        {
+            sensor.AddObservation(interrupteurTransform.localPosition);
+        } else
+        {
+            sensor.AddObservation(porteToReachTransform.localPosition);
+        }        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Porte1"))
+        if ((other.CompareTag("Porte1") && porte1Ouverte) || (other.CompareTag("Porte2") && porte2Ouverte))
         {
-            if (porte1Ouverte)
-            {
-                GameSuccess();
-            } else
-            {
-                GameOver("Porte");
-            }
-            
-        } 
-        else if (other.CompareTag("Porte2"))
+            GameSuccess();
+        }
+        else if ((other.CompareTag("Porte1") && !porte1Ouverte) || (other.CompareTag("Porte2") && !porte2Ouverte))
         {
-            if (porte2Ouverte)
-            {
-                GameSuccess();
-            }
-            else
-            {
-                GameOver("Porte");
-            }
-
+            GameOver("PorteFerme");
         }
         else if (other.CompareTag("Bouton"))
         {
@@ -94,97 +81,154 @@ public class AgentController : Agent
         else if (other.CompareTag("Mur"))
         {
             GameOver("Mur");
-        }      
+        }
+        else if (other.CompareTag("MurPorte"))
+        {
+            GameOver("MurPorte");
+        }
     }
+
 
     private void GameOver(string collider)
     {
-        solMaterial.color = Color.red;
-        float reward = -1f;
+        solMaterial.color = materialGameOver;
 
         if (collider == "Mur")
         {
-            reward = -3f;
+            AddReward(-100000f);
+        } else if(collider == "MurPorte")
+        {
+            AddReward(-10000f);
+            solMaterial.color = Color.blue;
         }
+            else if(collider == "PorteFerme")
+        {
+            AddReward(-100f);
+            solMaterial.color = Color.magenta;
+        } 
 
-        AddReward(reward);
         EndEpisode();
     }
 
     private void GameSuccess()
     {
-        solMaterial.color = Color.green;
+        solMaterial.color = materialGameSuccess;
 
-        AddReward(1000f);
+        SetReward(100000f);
         EndEpisode();
     }
 
     private void BoutonReached()
     {
-       
-                    
+        if (interrupteurAtteint)
+        {
+            SetReward(1000f);
+            indexPorteToReach = Random.Range(0, portes.Count);
+            porteToReach = portes[indexPorteToReach];
+            porteToReachMat = porteToReach.GetComponent<MeshRenderer>().material;
+            porteToReachTransform = porteToReach.transform;
+        } 
+        
+        interrupteurAtteint = !interrupteurAtteint;
+        interrupteurMaterial.color = interrupteurAtteint ? materialOn : materialOff;
+        porteToReachMat.color = interrupteurAtteint ? materialOff : materialOn;
 
-            if (indexPorteToReach == 0)
-            {
-                if (porte1Ouverte)
-                {
-                SetReward(-50f);
-                porte1Ouverte = false;
-                    porte1Material.color = Color.black;
-                } else
-                {
-                SetReward(100f);
-                porte1Ouverte = true;
-                    porte1Material.color = Color.yellow;
-                }
-                
-                
-            }
-            else
-            {
-            if (porte2Ouverte)
-            {
-                SetReward(-50f);
-                porte2Ouverte = false;
-                porte2Material.color = Color.black;
-            }
-            else
-            {
-                SetReward(100f);
-                porte2Ouverte = true;
-                porte2Material.color = Color.yellow;
-            }
+        if (indexPorteToReach == 0)
+        {
+            porte1Ouverte = !porte1Ouverte;
         }
-
-                    
+        else
+        {
+            porte2Ouverte = !porte2Ouverte;
+        }
     }
+
 
     public override void OnEpisodeBegin()
     {
-        indexPorteToReach = Random.Range(0, 2);
-        porteToReach = portes[indexPorteToReach];
-        porteToReachMat = porteToReach.GetComponent<MeshRenderer>().material;
+        // Initialisation de l'interrupteur
+        interrupteurMaterial = interrupteur.GetComponent<MeshRenderer>().material;
+        interrupteurTransform = interrupteur.transform;
+        interrupteurMaterial.color = materialOn;
+        interrupteurAtteint = true;
 
-        boutonMaterial.color = Color.yellow;
-        porte1Material.color = Color.black;
-        porte2Material.color = Color.black;
-
+        // Initialisation des portes
+        porte1Material = portes[0].GetComponent<MeshRenderer>().material;
+        porte1Transform = portes[0].transform;
         porte1Ouverte = false;
+
+        porte2Material = portes[1].GetComponent<MeshRenderer>().material;
+        porte2Transform = portes[1].transform;
         porte2Ouverte = false;
 
-        transform.localPosition = new Vector3(1, 0.34f, -3f);
-        //Vector3 move = new Vector3(Random.Range(-2f, 3f), 0, Random.Range(-3f, -2f));
-        //transform.Translate(move);
+        // Choix aléatoire de la porte à atteindre
+        porteToReach = portes[0];
+        porteToReachMat = porteToReach.GetComponent<MeshRenderer>().material;
+        porteToReachTransform = porteToReach.transform;
 
-        porte1Transform.localPosition = new Vector3(-1.5f, 1f, 3.5f);
+        // Initialisation du sol
+        solMaterial = sol.GetComponent<MeshRenderer>().material;
 
-        boutonTransform.localPosition = new Vector3(-0.5f, 0.2f, -2f);
+        // Position initiale de l'agent et de l'interrupteur
+        Vector3[] agentAndSwitchPositions = GeneratePositions(0.56f, 0.2f, -3.5f, 1.5f, 2f);
+        //transform.localPosition = agentAndSwitchPositions[0];
+        //interrupteurTransform.localPosition = agentAndSwitchPositions[1];
+        transform.localPosition = new Vector3(-1.5f, 0.56f, -0.45f);
+        interrupteurTransform.localPosition = new Vector3(-0.2f, 0.2f, 1.2f);
+
+        // Couleur initiales de l'interrupteur
+        interrupteurMaterial.color = materialOn;
+
+        // Position initiale des portes
+        Vector3[] doorPositions = GeneratePositions(1f, 0.5f, 4.10f, 4.10f, 2f);
+        porte1Transform.localPosition = doorPositions[0];
+        porte2Transform.localPosition = doorPositions[1];
+        //porte1Transform.localPosition = new Vector3(-1.5f, 1f, 4.1f);
+        //porte2Transform.localPosition = new Vector3(1.75f, 0.5f, 4.1f);
+
+        // Initialisation de la couleur des portes
+        porte1Material.color = materialOff;
+        porte2Material.color = materialOff;
     }
+
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> contActions = actionsOut.ContinuousActions;
         contActions[0] = Input.GetAxisRaw("Horizontal");
         contActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private Vector3[] GeneratePositions(float minY, float maxY, float minZ, float maxZ, float minDistance)
+    {
+        Vector3[] positions = new Vector3[2];
+
+        // Génération de la position x pour la première position
+        float position1X = Random.Range(-2f, 2f);
+        float position2X;
+
+        // Génération de la position x pour la deuxième position sans superposition avec la première
+        do
+        {
+            position2X = Random.Range(-2f, 2f);
+        } while (Mathf.Abs(position1X - position2X) < minDistance); // Vérification de la distance minimale entre les positions
+
+        float position1Z = minZ;
+        float position2Z = maxZ;
+
+        if (position1Z != position2Z)
+        {
+            position1Z = Random.Range(minZ, maxZ);
+            do
+            {
+                position2Z = Random.Range(minZ, maxZ);
+            } while (Mathf.Abs(position1Z - position2Z) < minDistance);
+        }
+
+        // Assignation des positions y et z pour les deux positions
+        positions[0] = new Vector3(position1X, minY, position1Z);
+        positions[1] = new Vector3(position2X, maxY, position2Z);
+
+        return positions;
     }
 }
